9c0db27f46f59aac557bdc4d17d9b909c2642403
Stas Sergeev <stsp@users.sourceforge.net>
2024-12-24 15:31:07 +0300

    adapt

--- packages/dosemu2/build.sh
+++ packages/dosemu2/build.sh
@@ -6,12 +6,12 @@ TERMUX_PKG_VERSION="2.0pre9-git"
 TERMUX_PKG_SRCURL=git+https://github.com/dosemu2/dosemu2.git
 TERMUX_PKG_GIT_BRANCH=devel
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc, fdpp, dj64dev"
-TERMUX_PKG_DEPENDS="fdpp, dj64dev, comcom64"
+TERMUX_PKG_BUILD_DEPENDS="libandroid-posix-semaphore, libandroid-glob, slang, libao, fluidsynth, ladspa-sdk, libslirp, libbsd, readline, json-c, libseccomp, libsearpc"
 # LTO breaks on i386 build
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-lto"
 
 termux_step_pre_configure() {
+	false
 	cd $TERMUX_PKG_SRCDIR
 	./autogen.sh
 }
